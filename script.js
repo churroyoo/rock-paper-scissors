2cd724a1bbd92df76830656ef950ba6019ba6158
Charles <skcmhm@gmail.com>
2022-12-14 20:41:00 -0800

    Create main game for loop game() loop will allow for 5 tries. Each try will call upon playRound function, which will do the neccessary calculations and then output score, return scores for the appropriate side and reset user and computer's decision.

--- script.js
+++ script.js
@@ -1,45 +1,71 @@
-const choices = [ "Rock", "Paper", "Scissors" ];
-let userInput = prompt("Rock, Paper, Scissors");
 
-//CHOICES: Prompts user choice input and Computer decides
-let getPlayerChoice = function () {
+//Prompts user choice input 
+function getPlayerChoice() {
+    let userInput = prompt("Rock, Paper, Scissors");
     return userInput.charAt(0).toUpperCase() + userInput.slice(1).toLowerCase();
 }
 
-let  getComputerChoice = function() {
-    //randomly assign a value from [choices]
+//Computer decides
+function getComputerChoice() {
+    const choices = [ "Rock", "Paper", "Scissors" ];
     let index = Math.floor(Math.random()*3) 
     return choices[index]
 }
 
-const playerSelection = getPlayerChoice();
-const computerSelection = getComputerChoice();
-
+//Play a round of RPS
 function playRound(playerSelection, computerSelection){
 
+    console.log("You chose: " + playerSelection)
+    console.log("The enemy chose: " + computerSelection)
 
     if (playerSelection === computerSelection) {
-        return "tie";
+        console.log("You tied this round")
+        score();
+        resetChoices();
+        return;
     }
-
     else {
         if ((playerSelection === "Rock" && computerSelection === "Paper")
         || (playerSelection === "Paper" && computerSelection === "Scissors")
         || (playerSelection === "Scissors" && computerSelection === "Rock")){
-            return "u lose";
+            console.log("You lost this round")
+            score();
+            resetChoices();
+            
+            return computerScore += 1;
         }
-        /*else if (playerSelection === "Paper" && computerSelection === "Scissors"){
-            return "u lose";
-        }
-        else if (playerSelection === "Scissors" && computerSelection === "Rock"){
-            return "u lose";
-        }*/
+
         else {
-            return "u win";
+            console.log("You won this round")
+            score();
+            resetChoices();
+            
+            return playerScore += 1;
         }
     }
 }
-console.log("You chose: " + playerSelection)
-console.log("The enemy chose: " + computerSelection)
 
-console.log(playRound( playerSelection, computerSelection ));
+//Displays score count
+function score(a = playerScore, b = computerScore){
+    console.log("You're score: " + a + "\n" + "Enemy score: " + b)
+}
+
+//Reset choices
+function resetChoices(){
+    playerChoice = getPlayerChoice();
+    computerChoice = getComputerChoice();
+}
+
+//Game Algorithm
+function game(){
+    for (let i = 0; i < 5; i++){
+        playRound(playerChoice, computerChoice);
+    }
+}
+
+let playerScore = 0; 
+let computerScore = 0;
+let playerChoice = getPlayerChoice();
+let computerChoice = getComputerChoice();
+
+game();
