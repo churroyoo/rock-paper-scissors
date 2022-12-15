a14366d89de54f7a0b9e92bec68e4cc1f2a37076
Charles <skcmhm@gmail.com>
2022-12-14 16:20:39 -0800

    Add user selection input

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 const choices = [ "Rock", "Paper", "Scissors" ]
+const userInput = prompt("Rock, Paper, Scissors");
+
+//CHOICES: Prompts user choice input and Computer decides
+function getPlayerChoice() {
+    return playerSelection = userInput.charAt(0).toUpperCase() + userInput.slice(1);
+}
 
 function getComputerChoice() {
-    //randomly assign a value from Rock Paper Scissors
+    //randomly assign a value from [choices]
     let index = Math.floor(Math.random()*3) 
     return choices[index]
 }
 
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === computerSelection) {
+
+    }
+
+    return 
+}
+
+function game(){
+
+}
+console.log(getPlayerChoice())
 console.log(getComputerChoice())
