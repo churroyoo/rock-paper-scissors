aa6b5937405cf4b31282ac1d77f91d0f05486ccc
Churro <27313833+churrou@users.noreply.github.com>
2023-11-27 13:44:21 -0800

    Delete standardization algorithm and create main()
    standardization was redundant and actually made
    the code harder to read
    + main loop

--- script.js
+++ script.js
@@ -1,48 +1,51 @@
 // The computer will make a random selection between rock paper scissors
-let computerSelection = () => numberToChoice(generateNumber())
-
 let generateNumber = () => Math.ceil(Math.random() * 3)
-
 let numberToChoice = (number) => {
     let choice = ""
-
+    
     switch (number){
         case 1:
-        choice = "rock"
-        break;
-
-        case 2:
-        choice = "paper"
-        break;
-
-        case 3:
-        choice = "scissors"
-        break;
-    } 
-
-    return choice
+            choice = "rock"
+            break;
+            
+            case 2:
+                choice = "paper"
+                break;
+                
+                case 3:
+                    choice = "scissors"
+                    break;
+                } 
+                
+                return choice
 }
+            
+let computerSelection = () => numberToChoice(generateNumber())
 
 // The player will input a command (string)
-let playerSelection = prompt("Make a selection: Rock, Paper, Scissors", "paper")
 // The code should standardize the command and make it case insensitive
-let standardizeSelection = (selection) => selection.toLowerCase()
+let playerSelection = () => prompt("Make a selection: Rock, Paper, Scissors", "paper").toLowerCase()
 
-console.log(standardizeSelection(playerSelection))
-console.log(computerSelection())
+
 // The code should check to see if computer and player both have a selection
+// The code will compare to see who won
+// The code will declare a winner
 function playRound(playerSelection, computerSelection){
     if (playerSelection === computerSelection){
-        console.log("It's a tie!")
+        return "It's a tie!"
     }else if 
     (playerSelection === "rock" && computerSelection === "paper" ||
     playerSelection === "scissors" && computerSelection === "rock" ||
     playerSelection === "paper" && computerSelection === "scissors" ){
-        console.log("You lost!")
+        return "You lost!"
     }else{
-        console.log("You won!")
+        return "You won!"
     }
 }
-// The code will compare to see who won
-// The code will declare a winner
+
 // The winner will increment in their score value
+function mainGame() {
+    for (let round = 0; round < 5; round++)
+    console.log(playRound(playerSelection(), computerSelection()))
+}
+mainGame()
