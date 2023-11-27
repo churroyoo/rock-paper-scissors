a921ebe7306ca77d2534916aa37c6e0fb1997114
Churro <27313833+churrou@users.noreply.github.com>
2023-11-27 10:41:26 -0800

    Create selection standarization algorithm

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 // The player will input a command (string)
 let playerSelection = prompt("Make a selection: Rock, Paper, Scissors", "paper")
 // The code should standardize the command and make it case insensitive
+let standardizeSelection = (selection) => {
+    selection.toLowerCase()
+}
 // The code should check to see if computer and player both have a selection
 // The code will compare to see who won
 // The code will declare a winner
