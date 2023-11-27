8c899ec6f06de643795cb914f771a2d6601bd0ba
Churro <27313833+churrou@users.noreply.github.com>
2023-11-27 11:12:24 -0800

    Create algorithms for computerSelection
    wrote 2 different functions to generate a random number and
    convert that number to a string we needed, all in the name
    of code readability

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 // The computer will make a random selection between rock paper scissors
+let computerSelection = () => numberToChoice(generateNumber())
+
+let generateNumber = () => Math.ceil(Math.random() * 3)
+
+let numberToChoice = (number) => {
+    let choice = ""
+
+    switch (number){
+        case 1:
+        choice = "rock"
+        break;
+
+        case 2:
+        choice = "paper"
+        break;
+
+        case 3:
+        choice = "scissors"
+        break;
+    } 
+
+    return choice
+}
+
 // The player will input a command (string)
 let playerSelection = prompt("Make a selection: Rock, Paper, Scissors", "paper")
 // The code should standardize the command and make it case insensitive
-let standardizeSelection = (selection) => {
-    selection.toLowerCase()
-}
+let standardizeSelection = (selection) => selection.toLowerCase()
+console.log(standardizeSelection(playerSelection))
+console.log(computerSelection())
 // The code should check to see if computer and player both have a selection
 // The code will compare to see who won
 // The code will declare a winner
