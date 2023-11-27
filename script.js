91c060e7201d057e1f65d24bf9c552bc1a0aa1bb
Churro <27313833+churrou@users.noreply.github.com>
2023-11-27 10:39:12 -0800

    Make player selection input form

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-/*
-The computer will make a random selection between rock paper scissors
-The player will input a command (string)
-The code should standardize the command and make it case insensitive
-The code should check to see if computer and player both have a selection
-The code will compare to see who won
-The code will declare a winner
-The winner will increment in their score value
-*/
+// The computer will make a random selection between rock paper scissors
+// The player will input a command (string)
+let playerSelection = prompt("Make a selection: Rock, Paper, Scissors", "paper")
+// The code should standardize the command and make it case insensitive
+// The code should check to see if computer and player both have a selection
+// The code will compare to see who won
+// The code will declare a winner
+// The winner will increment in their score value
