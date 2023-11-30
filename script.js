3573e19a371219b410757698a139da2d5b5225bd
Churro <27313833+churrou@users.noreply.github.com>
2023-11-29 22:10:10 -0800

    Delete everything and start from fresh
    while the code worked, everything was intertwining
    and getting too messy

--- script.js
+++ script.js
@@ -20,54 +20,29 @@ let numberToChoice = (number) => {
 }
 let computerSelection = () => numberToChoice(generateNumber())
 
-// The player will input a command (string)
-// The code should standardize the command and make it case insensitive
+
 let playerSelection;
 let buttons = document.querySelector("#buttons")
 
 buttons.addEventListener("click", (event) => {
-    let target = event.target.id
-    let display = document.querySelector("#results")
-    let resultTextBox = document.createElement("p")
-    let winner;
+    let targetId = event.target.id
 
-    switch (target) {
+    switch (targetId) {
         case "rock":
-        playerSelection = "rock"
-        winner = playRound(playerSelection, computerSelection())
-        resultTextBox.textContent = `
-        You chose: ${playerSelection}
-        PC chose: ${computerSelection}
-        ${winner}`
-        display.appendChild(resultTextBox)
+        
         break;
 
         case "paper":
-        playerSelection = "paper"        
-        winner = playRound(playerSelection, computerSelection())
-        resultTextBox.textContent = `
-        You chose: ${playerSelection}
-        PC chose: ${computerSelection}
-        ${winner}`
-        display.appendChild(resultTextBox)
+        
         break;
 
         case "scissors":
-        playerSelection = "scissors"
-        winner = playRound(playerSelection, computerSelection())
-        resultTextBox.textContent = `
-        You chose: ${playerSelection}
-        PC chose: ${computerSelection}
-        ${winner}`
-        display.appendChild(resultTextBox)
+        
         break;
     }
 });
 
 
-// The code should check to see if computer and player both have a selection
-// The code will compare to see who won
-// The code will declare a winner
 function playRound(playerSelection, computerSelection){
     if (playerSelection === computerSelection){
         return "It's a tie!"
@@ -80,23 +55,3 @@ function playRound(playerSelection, computerSelection){
         return "You won!"
     }
 }
-
-// The winner will increment in their score value
-
-// let results = playRound(playerSelection(), computerSelection())
-
-// if (results === "You lost!") {
-//     computerScore++
-//     console.log(`${results} 
-//     Your score:${playerScore} 
-//     PC's score:${computerScore} `)
-
-// }else if (results === "You won!") {
-//     playerScore++
-//     console.log(`${results} 
-//     Your score:${playerScore} 
-//     PC's score:${computerScore} `)
-// }else{
-//     console.log(`${results} 
-//     Your score:${playerScore} 
-//     PC's score:${computerScore} `)
