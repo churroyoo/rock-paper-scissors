aa1148e79287788863291aba353d74d42295769b
Churro <27313833+churrou@users.noreply.github.com>
2023-11-29 23:13:11 -0800

    Complete game loop
    the game loop for 1 iteration is complete

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const buttons = document.querySelector("#buttons")
+const resultDisplay = document.querySelector("#result")
+const playerScoreDisplay = document.querySelector("#player-score")
+const computerScoreDisplay = document.querySelector("#computer-score")
+
+let playerScore = 0;
+let computerScore = 0;
+let result = "";
+let gameOver = false;
 
 // The computer will make a random selection between rock paper scissors
 function genRandomNumber() {return Math.ceil(Math.random() * 3)}
@@ -21,25 +29,54 @@ function numberToChoice(number){
     return choice
 }
 function getComputerChoice() {return numberToChoice(genRandomNumber())}
-
-// The player will press a button
-//we will save that buttons class as their choice
+//The players choice is taken from the buttons id using event delegation
 function getPlayerChoice(event) {return playerChoice = event.target.id;}
 
 buttons.addEventListener("click", (event) => {
-    console.log(getPlayerChoice(event))
+    checkGameOver()
+    if (gameOver){
+        resultDisplay.textContent = "Game Over!";
+        playerScoreDisplay.textContent = "Your Score: " + playerScore.toString();
+        computerScoreDisplay.textContent = "PC Score: " + computerScore.toString();
+    }else{
+        playRound(getPlayerChoice(event), getComputerChoice())
+        resultDisplay.textContent = result;
+        playerScoreDisplay.textContent = "Your Score: " + playerScore.toString();
+        computerScoreDisplay.textContent = "PC Score: " + computerScore.toString();
+    }
 });
-
 
 function playRound(playerSelection, computerSelection){
     if (playerSelection === computerSelection){
-        return "It's a tie!"
+        result = "You tied!";
+        return
     }else if 
     (playerSelection === "rock" && computerSelection === "paper" ||
     playerSelection === "scissors" && computerSelection === "rock" ||
     playerSelection === "paper" && computerSelection === "scissors" ){
-        return "You lost!"
+        result = "The PC got a point!"
+        computerScore++
+        checkWinner()
+        return
     }else{
-        return "You won!"
+        result = "You got a point!"
+        playerScore++
+        checkWinner()
+        return
     }
+}
+function checkGameOver() {
+    if (playerScore >= 5 || computerScore >= 5){
+        gameOver = true;
+    }else{
+        gameOver = false;
+    }
+}
+function checkWinner(){
+    if (playerScore === 5) {
+        result = "CONGRATS, YOU WON"
+    } else if (computerScore === 5){
+        result = "Sorry, You lost..."
+    }
+    return gameOver = true
 }
